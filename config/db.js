37d7db019062b8b88dc3067839c18dd76a3f0496
[Arun Pandian] <[arun07160@gmail.com]>
2023-12-28 20:28:06 +0530

    added

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const connection=mongoose.createConnection('mongodb://127.0.0.1:27017/url_short')
+const connection=mongoose.createConnection('mongodb://127.0.0.1:27017')
 connection.on('connected',()=>{
     console.log("Db connected");
 })
